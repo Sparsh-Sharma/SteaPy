2d220976924bea683dbb1d1882e4b603b8cecba7
Sparsh <sparsh.sharma@mail.ru>
2017-04-18 20:09:37 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+The :mod:`steapy` package implements a boundary element method for steady inviscid
+fluid flows.
+"""
+
+from .airfoil_generate import *
+from .CORNEAL_toolbox import *
+
+
+__version__ = "0.1"
